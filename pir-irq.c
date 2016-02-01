829364e04b5622d7b9de7ea3df174d8e951fdb80
alanb <efik722life@gmail.com>
2016-02-01 12:08:27 +0900

    fixed PIR timeout logic

--- pir-irq.c
+++ pir-irq.c
@@ -5,8 +5,11 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/time.h>
+#include <linux/timer.h>
+#include <linux/types.h>
 #include <linux/suspend.h>
 #include <linux/pm.h>
+#include <linux/pm_runtime.h>
 #include <linux/pm_wakeup.h>
 #include <linux/pm_wakeirq.h>
 #include <linux/workqueue.h>
@@ -15,20 +18,25 @@
 #define DEVICE_NAME "pir"
 #define IR_GPIO_PORT 28
 
-const static struct timespec waiting_time = {
-    .tv_sec = 5,
-    .tv_nsec = 0
-};
+const static unsigned long timeout_sec = 5*HZ;
 
+/* device callbacks */
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
+/* system syspend callbacks */
 static int suspend_valid(suspend_state_t state);
 static int suspend_enter(suspend_state_t state);
 
+/* device power management callbacks */
 static int device_on_runtime_suspend(struct device *dev);
+
+/* timer helper functions */
+static void pir_timer_register(struct timer_list *ptimer, unsigned long timeover);
+static void pir_timer_timeover(unsigned long arg);
+static void pir_timer_delete(struct timer_list *ptimer);
 
 static struct class *device_class;
 static struct device *pir_device;
@@ -41,7 +49,7 @@ static char waiting_bit = 0;
 
 static int pir_int_num;
 
-static struct timespec time_last_zero_bit;
+static struct timer_list pir_timer;
 
 static struct file_operations fops = {
     .read = device_read,
@@ -59,12 +67,14 @@ static struct dev_pm_ops dpmops = {
     .runtime_suspend = device_on_runtime_suspend
 };
 
+static struct dev_pm_domain dpmdomain;
+
 static void suspend_func(struct work_struct *dummy)
 {
     int ret;
     if ((ret = pm_suspend(PM_SUSPEND_MEM)) < 0)
     {
-        printk("pm_suspend failed, ret=%d", ret);
+        printk("pm_suspend failed, ret=%d\n", ret);
     }
 }
 
@@ -87,9 +97,9 @@ static int device_on_runtime_suspend(struct device *dev)
         printk("ERROR at enable_irq_wake: error code %d\n", ret);
         return ret;
     }
-    dev_pm_enable_wake_irq(pir_device);
+    dev_pm_enable_wake_irq(dev);
 
-    return 0;
+    return -1;
 }
 
 static int suspend_enter(suspend_state_t state)
@@ -103,15 +113,11 @@ irqreturn_t irq_handler(int irq, void *dev_id)
     if (event_detected)
     {
         waiting_bit = 1;
+        pir_timer_delete(&pir_timer);
     }
     if (waiting_bit && !event_detected)
     {
-        time_last_zero_bit = current_kernel_time();
-    }
-
-    if (!event_detected)
-    {
-        schedule_work(&suspend_work);
+        pir_timer_register(&pir_timer, timeout_sec);
     }
 
     return IRQ_HANDLED;
@@ -119,6 +125,8 @@ irqreturn_t irq_handler(int irq, void *dev_id)
 
 static int __init init_pir_module(void)
 {
+    int ret;
+
     /* character device creation */
     major_num = register_chrdev(0, DEVICE_NAME, &fops);
     if (major_num < 0)
@@ -139,7 +147,7 @@ static int __init init_pir_module(void)
     pir_device = device_create(device_class, NULL, devdev, NULL, DEVICE_NAME);
 
     /* PIR GPIO registration */
-    int ret = gpio_request(IR_GPIO_PORT, "IR GPIO");
+    ret = gpio_request(IR_GPIO_PORT, "IR GPIO");
     if (ret < 0)
     {
         printk("ERROR: cannot request GPIO %d: error code %d\n", IR_GPIO_PORT, ret);
@@ -157,16 +165,16 @@ static int __init init_pir_module(void)
     printk("Interrupt for GPIO %d: %d\n", IR_GPIO_PORT, pir_int_num);
 
     /* ISR registration */
-    int request_ret = request_irq(pir_int_num,
+    ret = request_irq(pir_int_num,
             irq_handler,
             IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
             "PIR IRQ handler",
             NULL);
 
-    if (request_ret != 0)
+    if (ret != 0)
     {
-        printk("ERROR: cannot request IRQ %d, error code %d\n", pir_int_num, request_ret);
-        return request_ret;
+        printk("ERROR: cannot request IRQ %d, error code %d\n", pir_int_num, ret);
+        return ret;
     }
 
     /* device wakeup setting */
@@ -190,6 +198,9 @@ static int __init init_pir_module(void)
         return -1;
     }
 
+    dpmdomain.ops = dpmops;
+    pir_device->pm_domain = &dpmdomain;
+
     /* suspend opeartions setup */
     suspend_set_ops(&sops);
 
@@ -198,6 +209,7 @@ static int __init init_pir_module(void)
 
 static void __exit exit_pir_module(void)
 {
+    pir_timer_delete(&pir_timer);
     free_irq(pir_int_num, NULL);
     gpio_free(IR_GPIO_PORT);
     device_destroy(device_class, devdev);
@@ -235,17 +247,6 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_
         bytes_to_read--;
         length--;
         
-        if (!event_detected && waiting_bit)
-        {
-            struct timespec interval_after_zero = timespec_sub(current_kernel_time(), time_last_zero_bit);
-
-            if (interval_after_zero.tv_sec > waiting_time.tv_sec || (interval_after_zero.tv_sec == waiting_time.tv_sec &&
-                        interval_after_zero.tv_nsec >= waiting_time.tv_nsec))
-            {
-                waiting_bit = 0;
-            }
-        }
-
         put_user(waiting_bit, buffer++);
         bytes_to_read--;
         length--;
@@ -258,6 +259,32 @@ static ssize_t device_write(struct file *filp, const char *buffer, size_t length
 {
     printk("write not offered by %s\n", DEVICE_NAME);
     return -EINVAL;
+}
+
+static void pir_timer_register(struct timer_list *ptimer, unsigned long timeover)
+{
+    init_timer(ptimer);
+    ptimer->expires = get_jiffies_64() + timeover;
+    ptimer->data = NULL;
+    ptimer->function = pir_timer_timeover;
+    add_timer(ptimer);
+}
+
+static void pir_timer_timeover(unsigned long arg)
+{
+    printk("PIR motion detection timeout!\n");
+    waiting_bit = 0;
+    pir_timer_delete(&pir_timer);
+//    schedule_work(&suspend_work);
+}
+
+static void pir_timer_delete(struct timer_list *ptimer)
+{
+    if (ptimer)
+    {
+        del_timer(ptimer);
+        ptimer = NULL;
+    }
 }
 
 module_init(init_pir_module);
