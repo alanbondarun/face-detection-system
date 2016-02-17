8effe7be82d5a06bde208e21fa9d8b93850c94b9
alanb <efik722life@gmail.com>
2016-02-18 00:31:15 +0900

    interface and constructor implementation of Network

--- include/network.hpp
+++ include/network.hpp
@@ -1,11 +1,62 @@
 #ifndef __NETWORK_HPP
 #define __NETWORK_HPP
 
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include "json/json.h"
+#include "layers/layer.hpp"
+#include "layers/layer_data.hpp"
+
 namespace NeuralNet
 {
 	class Network
 	{
+	public:
+		/* enum for input type */
+		enum class InputType
+		{
+			VECTOR, IMAGE
+		};
+	
+		/* an exception thrown when this class received invalid JSON data */
+		class InvalidJSONException
+		{
+		public:
+			InvalidJSONException(const std::string& region)
+				: m_reg(std::string("Invalid JSON setting") + region) {}
+			virtual ~InvalidJSONException() {}
+			virtual const char* what() const { return m_reg.c_str(); }
+			
+		private:
+			const std::string m_reg;
+		};
+	
+		Network(const Json::Value& setting);
+		~Network();
 		
+		// returns classification value for one portion of data
+		std::vector<int> evaluate(const double *data) const;
+		
+		// trains with m_train_size number of data
+		void train(const double *data);
+		
+	private:
+		struct Node;
+		
+		void addLayer(const Json::Value& jsonLayer);
+		std::vector<Node *> feedForward(Node *in);
+		Node *backPropagate(Node *in);
+		
+		InputType m_in_type;
+		struct InputSize
+		{
+			size_t size;
+			size_t width, height;
+		} m_in_size;
+		size_t m_train_size, m_batch_size;
+		
+		Node *root;
 	};
 }
 
--- src/network.cpp
+++ src/network.cpp
@@ -1,6 +1,82 @@
+#include <string>
 #include "network.hpp"
+#include "utils/make_unique.hpp"
 
 namespace NeuralNet
 {
+	struct Network::Node
+	{
+		std::unique_ptr<Layer> layer;
+		std::unique_ptr<LayerData> data;
+		std::vector<Node *> next;
+		Node *prev;
+	};
 	
+	/* may emit Json::Exception while execution */
+	Network::Network(const Json::Value& setting)
+		: root(nullptr)
+	{
+		/* input file description */
+		auto input_val = setting["input"];
+		auto input_type_str = input_val["type"].asString();
+		if (!input_type_str.compare("vector"))
+		{
+			m_in_type = InputType::VECTOR;
+			m_in_size.size = input_val["size"].asUInt();
+		}
+		else if (!input_type_str.compare("image"))
+		{
+			m_in_type = InputType::IMAGE;
+			auto input_size = input_val["size"];
+			m_in_size.width = input_size["width"].asUInt();
+			m_in_size.height = input_size["height"].asUInt();
+		}
+		else
+			throw Json::LogicError("invalid input.type string");
+		
+		/* train size and batch size */
+		m_train_size = setting["train_num"].asUInt();
+		m_batch_size = setting["batch_size"].asUInt();
+		
+		/* layer construction */
+		auto layers = setting["layers"];
+		if (layers.size() <= 0)
+			throw Json::LogicError("there must be at least one layer");
+
+		auto layer1 = layers[0u];
+		addLayer(layer1);
+		for (size_t idx = 1; idx < layers.size(); idx++)
+		{
+			addLayer(layers[idx]);
+		}
+	}
+	
+	void Network::addLayer(const Json::Value& jsonLayer)
+	{
+		
+	}
+	
+	Network::~Network()
+	{
+		
+	}
+	
+	std::vector<int> Network::evaluate(const double *data) const
+	{
+	}
+	
+	void Network::train(const double *data)
+	{
+		
+	}
+	
+	std::vector<Network::Node *> Network::feedForward(Network::Node *in)
+	{
+		
+	}
+	
+	Network::Node* Network::backPropagate(Network::Node *in)
+	{
+		
+	}
 }
