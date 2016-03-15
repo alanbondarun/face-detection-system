59bcb8ecbdfdfa269e8ac70d1c506cc5f0852301
alanb <efik722life@gmail.com>
2016-03-15 23:27:30 +0900

    change opencl test code to cpp wrappers

--- src/test/test_cl.cpp
+++ src/test/test_cl.cpp
@@ -3,180 +3,52 @@
 #include <string>
 #include <fstream>
 
-#ifdef __APPLE__
-    #include "OpenCL/opencl.h"
-#else
-    #include "CL/cl.h"
-#endif
+#define __CL_ENABLE_EXCEPTIONS
+#include "CL/cl.hpp"
 
-std::string GetPlatformName (cl_platform_id id)
-{
-    size_t size = 0;
-    clGetPlatformInfo (id, CL_PLATFORM_NAME, 0, nullptr, &size);
-
-    std::string result;
-    result.resize (size);
-    clGetPlatformInfo (id, CL_PLATFORM_NAME, size,
-        const_cast<char*> (result.data ()), nullptr);
-
-    return result;
-}
-
-std::string GetDeviceName (cl_device_id id)
-{
-    size_t size = 0;
-    clGetDeviceInfo (id, CL_DEVICE_NAME, 0, nullptr, &size);
-
-    std::string result;
-    result.resize (size);
-    clGetDeviceInfo (id, CL_DEVICE_NAME, size,
-        const_cast<char*> (result.data ()), nullptr);
-
-    return result;
-}
-
-void CheckError (cl_int error)
-{
-    if (error != CL_SUCCESS) {
-        std::cerr << "OpenCL call failed with error " << error << std::endl;
-        std::exit (1);
-    }
-}
-
-std::string LoadKernel (const char* name)
-{
-    std::ifstream in (name);
-    std::string result (
-        (std::istreambuf_iterator<char> (in)),
-        std::istreambuf_iterator<char> ());
-    return result;
-}
-
-cl_program CreateProgram (const std::string& source,
-    cl_context context)
-{
-    size_t lengths [1] = { source.size () };
-    const char* sources [1] = { source.data () };
-
-    cl_int error = 0;
-    cl_program program = clCreateProgramWithSource (context, 1, sources, lengths, &error);
-    CheckError (error);
-
-    return program;
-}
+const char* helloStr = "__kernel void hello(void) { }";
 
 int main ()
 {
-    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetPlatformIDs.html
-    cl_uint platformIdCount = 0;
-    clGetPlatformIDs (0, nullptr, &platformIdCount);
-
-    if (platformIdCount == 0) {
-        std::cerr << "No OpenCL platform found" << std::endl;
-        return 1;
-    } else {
-        std::cout << "Found " << platformIdCount << " platform(s)" << std::endl;
-    }
-
-    std::vector<cl_platform_id> platformIds (platformIdCount);
-    clGetPlatformIDs (platformIdCount, platformIds.data (), nullptr);
-
-    for (cl_uint i = 0; i < platformIdCount; ++i) {
-        std::cout << "\t (" << (i+1) << ") : " << GetPlatformName (platformIds [i]) << std::endl;
-    }
-
-    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetDeviceIDs.html
-    cl_uint deviceIdCount = 0;
-    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_ALL, 0, nullptr,
-        &deviceIdCount);
-
-    if (deviceIdCount == 0) {
-        std::cerr << "No OpenCL devices found" << std::endl;
-        return 1;
-    } else {
-        std::cout << "Found " << deviceIdCount << " device(s)" << std::endl;
-    }
-
-    std::vector<cl_device_id> deviceIds (deviceIdCount);
-    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_ALL, deviceIdCount,
-        deviceIds.data (), nullptr);
-
-    for (cl_uint i = 0; i < deviceIdCount; ++i) {
-        std::cout << "\t (" << (i+1) << ") : " << GetDeviceName (deviceIds [i]) << std::endl;
-    }
-
-    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateContext.html
-    const cl_context_properties contextProperties [] =
+    cl_int err = CL_SUCCESS;
+    try
     {
-        CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties> (platformIds [0]),
-        0, 0
-    };
+        std::vector<cl::Platform> platforms;
+        cl::Platform::get(&platforms);
+        if (platforms.size() == 0) {
+            std::cout << "Platform size 0\n";
+            return -1;
+        }
 
-    cl_int error = CL_SUCCESS;
-    cl_context context = clCreateContext (contextProperties, deviceIdCount,
-        deviceIds.data (), nullptr, nullptr, &error);
-    CheckError (error);
+        cl_context_properties properties[] = 
+            { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0};
+        cl::Context context(CL_DEVICE_TYPE_CPU, properties); 
 
-    std::cout << "Context created" << std::endl;
+        std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
-    cl_program program = CreateProgram (LoadKernel ("kernels/saxpy.cl"),
-        context);
+        cl::Program::Sources source(1,
+            std::make_pair(helloStr, strlen(helloStr)));
+        cl::Program program_ = cl::Program(context, source);
+        program_.build(devices);
 
-    CheckError (clBuildProgram (program, deviceIdCount, deviceIds.data (), nullptr, nullptr, nullptr));
+        cl::Kernel kernel(program_, "hello", &err);
 
-    cl_kernel kernel = clCreateKernel (program, "SAXPY", &error);
-    CheckError (error);
+        cl::Event event;
+        cl::CommandQueue queue(context, devices[0], 0, &err);
+        queue.enqueueNDRangeKernel(
+            kernel, 
+            cl::NullRange, 
+            cl::NDRange(4,4),
+            cl::NullRange,
+            NULL,
+            &event); 
 
-    // Prepare some test data
-    static const size_t testDataSize = 1 << 10;
-    std::vector<float> a (testDataSize), b (testDataSize);
-    for (int i = 0; i < testDataSize; ++i) {
-        a [i] = static_cast<float> (23 ^ i);
-        b [i] = static_cast<float> (42 ^ i);
+        event.wait();
+    }
+    catch (cl::Error error)
+    {
+        std::cerr << "ERROR: " << error.what() << "(" << error.err() << ")" << std::endl;
     }
 
-    cl_mem aBuffer = clCreateBuffer (context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-        sizeof (float) * (testDataSize),
-        a.data (), &error);
-    CheckError (error);
-
-    cl_mem bBuffer = clCreateBuffer (context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
-        sizeof (float) * (testDataSize),
-        b.data (), &error);
-    CheckError (error);
-
-    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateCommandQueue.html
-    cl_command_queue queue = clCreateCommandQueue (context, deviceIds [0],
-        0, &error);
-    CheckError (error);
-
-    clSetKernelArg (kernel, 0, sizeof (cl_mem), &aBuffer);
-    clSetKernelArg (kernel, 1, sizeof (cl_mem), &bBuffer);
-    static const float two = 2.0f;
-    clSetKernelArg (kernel, 2, sizeof (float), &two);
-
-    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clEnqueueNDRangeKernel.html
-    const size_t globalWorkSize [] = { testDataSize, 0, 0 };
-    CheckError (clEnqueueNDRangeKernel (queue, kernel, 1,
-        nullptr,
-        globalWorkSize,
-        nullptr,
-        0, nullptr, nullptr));
-
-    // Get the results back to the host
-    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clEnqueueReadBuffer.html
-    CheckError (clEnqueueReadBuffer (queue, bBuffer, CL_TRUE, 0,
-        sizeof (float) * testDataSize,
-        b.data (),
-        0, nullptr, nullptr));
-
-    clReleaseCommandQueue (queue);
-
-    clReleaseMemObject (bBuffer);
-    clReleaseMemObject (aBuffer);
-
-    clReleaseKernel (kernel);
-    clReleaseProgram (program);
-
-    clReleaseContext (context);
+    return EXIT_SUCCESS;
 }
